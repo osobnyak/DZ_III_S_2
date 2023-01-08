dbe5967f922494cf95589c60cb6123ae55e4c09f
Vladimir.S <vibiray2@mail.ru>
2023-01-09 01:56:35 +0400

    2 done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿// 
+﻿// задание 13. выводить 3-ю цифру заданного числа или сообщаем, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n<100)
+{
+    Console.WriteLine("третьей цифры нет");    
+}
+
+else 
+{while (n>999)
+    n = n - 1000;
+    Console.WriteLine(n);   
+
+
+    int result1 = n / 100;
+
+    Console.Write("3-я цифра: ");
+    Console.WriteLine(result1); 
+}
